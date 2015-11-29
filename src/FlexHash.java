8eb78720ba297eddd806915941d182f47f182105
Scieon <anhkhoi.vunguyen@gmail.com>
2015-11-28 23:47:59 -0500

    Documented version

--- src/FlexHash.java
+++ src/FlexHash.java
@@ -1,100 +1,123 @@
 
 public class FlexHash {
 
-	private int entries;
-	private int capacity;
-	private String [] table;
-	private double threshold;
-	private double loadFactor;
-	private char collisionType;
-	private int colfreq = 0;
-	private int max = 0;
+	private int entries; //Number of elements in table.
+	private int capacity; //Maximum size of table.
+	private String [] table; 
+	private double threshold; //Limit of table maximum is 1 unless we do separate chaining.
+	private double loadFactor; //Number of elements / Capacity.
+	private char collisionType; 
+	private int colfreq = 0; //Number of collisions.
+	private int max = 0; //Maximum number of collisions.
+	
+	//Does quadratic
+	private int numColCell = 0;
 
-	public int colfreq(){
-		return colfreq;
-	}
+	
 	public FlexHash(){
 		entries = 0;
 		capacity = 101;
-		loadFactor = 0.6;
+		threshold = 0.6;
 		table = new String[capacity];
 	}
 
 	public void put(String key, String value){
 
+		int tempMax = 0;
+		
+		//Generating hash code.
 		int code = hashfun(key);
 
+		//If current spot was always empty.
 		if(table[code] == null){
-			//System.out.println("good");
+			
+			//Put value at the key.
 			table[code] = value;
-			++entries;
-			//System.out.println(entries);
-			threshold = (double)entries/capacity;
-			if(threshold >= loadFactor){
-				
-			//	System.out.println("rehasing1");
+			++entries; //Increase by 1 entry.
+			
+			//After adding we must verify if threshold has not been exceeded.
+			loadFactor = (double)entries/capacity;
+			if(loadFactor >= threshold){  //If Exceeded, rehash.
 				rehash();
 			}
-			return;
 		}
 
+		//A collision has occurred at this point.
 		else{
 			
 			int factor = 1;
 			//System.out.println("collide");
-			colfreq++;
+			colfreq++; //Number of collisions happening.
 			while(table[code] != null){
-		
+				tempMax++;
+				//Handling the collision by quadratic.
 				if(table[quadratic(code,factor)] == null){
 				
 					table[quadratic(code,factor)] = value;
 					++entries;
 
-					threshold = (double)entries/capacity;	
+					//After adding we must verify if threshold has not been exceeded.
+					loadFactor = (double)entries/capacity;	
 					
-					if(threshold >= loadFactor){
+					if(loadFactor >= threshold){
 						//System.out.println("rehasing2");
 						rehash();
-			
-						
 					}
 					break;
-				
 				}
 				factor++;
 			} //End of while
+			
+			if(tempMax > max)
+				max = tempMax;
 		}
 
 	}
 
+	//Quadratic Collision handling.
 	public int quadratic(int code, int factor){
-
 		return (code + factor*factor) % capacity;
 	}
 
+	
+	//Return number of collisions across entire table.
+	public int colfreq(){
+		return colfreq;
+	}
+	
+	//Return max number of collisions at a single bucket.
+	public int max(){
+		return max;
+	}
+	
+	//Ignore this method for now.
 	public String get(String key){
 
 		int code = hashfun(key);
 		return table[code];
 	}
 
+	//Rehash is done when it is time to increase the size of the table.
 	public void rehash(){
-	
-		colfreq = 0;
+		
+		colfreq = 0; //Resetting collision frequency since this is new table.
 		entries = 0; //Resetting Entries
-		int oldCap = this.capacity;
+		int oldCap = this.capacity; 
 		int newCap = 2*oldCap + 1;
-		this.capacity = newCap;
+		this.capacity = newCap; //Setting new Capacity
 
 		String [] oldTable = new String[oldCap];
 
+		//Storing a deep copy of old table.
 		for(int i=0; i<oldCap;i++){
 			if(table[i] != null)
 				oldTable[i] = table[i];
 		}
 
+		//Setting size of new table.
 		table = new String[newCap];
 
+		//Hashing old table into new table.
 		for(int i=0; i<oldCap;i++){
 
 			if(oldTable[i] != null)
@@ -105,9 +128,12 @@ public class FlexHash {
 
 	}
 
+	//Return number of elements in table.
 	public int entries(){
 		return entries;
 	}
+	
+	//Return capacity of table.
 	public int size(){
 		return capacity;
 	}
@@ -116,12 +142,14 @@ public class FlexHash {
 		System.out.println("----------------------");
 		System.out.println("Size of Table: " + capacity);
 		System.out.println("Number of Elements: " + entries);
-		System.out.println("Load Percentage: " + loadFactor);
+		System.out.println("Load Percentage: " + threshold);
 		System.out.println("Total Collisions: " + colfreq);
-		System.out.println("Max: ?");
+		System.out.println("Max: " + max);
 		System.out.println("Average: ?");
 		
 	}
+	
+	//Generate a hash value based of String key.
 	public int hash(String key){
 
 		int hash = 7;
@@ -132,6 +160,7 @@ public class FlexHash {
 		return hash;
 	}
 
+	//Compress the hash value.
 	public int compress(int hash){
 
 		int val = hash;
@@ -142,6 +171,7 @@ public class FlexHash {
 		return val%capacity;
 	}
 
+	//Hashfunction geenrating hash code based off String key.
 	public int hashfun(String key){
 		return compress(hash(key));
 	}
